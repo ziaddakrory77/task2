951ecc17fa1f3004e15b71837925dfaba7ae1d30
ziaddakrory77 <ziaddakrory8@gmail.com>
2025-07-02 01:56:28 +0300

    Add files via upload

---
+++ task2/tassk2.py
@@ -0,0 +1,6 @@
+student_list =[ {"name1" : "ziad" , "grades" : [ 50 ,44 ,47 ]} ,
+               {"name2" : "mariem" , "grades" : [ 43 ,39 ,45 ]} ,
+               {"name3" : " ahmed" , "grades" : [ 48 ,46 ,25]} ]
+for student in student_list:
+ avreage = (student_list["grades"]) / len(student_list["grades"]) 
+
